c3492d0e72b80cccbfc1df9b6f674d9f6440de14
NikitaKogtev <nikitakogtev@gmail.com>
2024-01-13 13:20:27 +0700

    Initial project

---
+++ src/main/java/ru/kogtev/FilterConsole.java
@@ -0,0 +1,19 @@
+package ru.kogtev;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class FilterConsole {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world");
+    }
+}
+
+
